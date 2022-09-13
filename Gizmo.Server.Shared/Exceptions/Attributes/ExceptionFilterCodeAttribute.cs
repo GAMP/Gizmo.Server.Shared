82b4bd66792841b89e412697dfa2ef175cd5afd0
Oleg Savelos <olegsavelos@gmail.com>
2022-09-13 15:21:52 +0300

    Remove serialization attributes.

--- Gizmo.Server.Shared/Exceptions/Attributes/ExceptionFilterCodeAttribute.cs
+++ Gizmo.Server.Shared/Exceptions/Attributes/ExceptionFilterCodeAttribute.cs
@@ -1,16 +1,10 @@
-﻿using Gizmo.Server.Exceptions.Errors;
-using Gizmo.Shared.Exceptions;
-using System;
-using System.Runtime.Serialization;
-using Gizmo.Server.Web.Api;
+﻿using Gizmo.Server.Web.Api;
 
 namespace Gizmo.Server.Exceptions.Attributes
 {
     /// <summary>
     /// Asset exception.
     /// </summary>
-    [Serializable()]
-    [DataContract()]
     public class ExceptionFilterCodeAttribute : Attribute
     {
         public WebApiErrorCode ErrorCode { get; set; }
