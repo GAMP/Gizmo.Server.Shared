4e15db625525283ba9df7dd341c9c431bb4a8fab
Oleg Savelos <olegsavelos@gmail.com>
2023-09-12 15:56:24 +0300

    updates.

---
+++ Gizmo.Server.Shared/Exceptions/AssistanceRequestException.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Serialization;
+using Gizmo.Exceptions;
+
+namespace Gizmo.Server.Exceptions
+{
+    [Serializable()]
+    [ExceptionFilterCode(ExceptionCode.AssistanceRequest)]
+    public class AssistanceRequestException : ErrorCodeExceptionBase<AssistanceRequestErrorCode>
+    {
+        #region CONSTRUCTOR
+
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        /// <param name="errorCode">Error code.</param>
+        public AssistanceRequestException(AssistanceRequestErrorCode errorCode) : base(errorCode)
+        { }
+
+        /// <summary>
+        /// Serialization constructor.
+        /// </summary>
+        /// <param name="info">Serialization info.</param>
+        /// <param name="context">Serialization context.</param>
+        protected AssistanceRequestException(SerializationInfo info,
+           StreamingContext context)
+            : base(info, context)
+        { }
+
+        #endregion
+    }
+}
---
+++ Gizmo.Server.Shared/Exceptions/Errors/AssistanceRequestErrorCode.cs
@@ -0,0 +1,31 @@
+﻿namespace Gizmo.Server.Exceptions
+{
+    public enum AssistanceRequestErrorCode
+    {
+        /// <summary>
+        /// Another assistance request is already pending or being processed.
+        /// </summary>
+        AlreadyPending = 0,
+        /// <summary>
+        /// Indicates that user is not logged in into host.
+        /// </summary>
+        /// <remarks>
+        /// Host is required when creating assistance request.
+        /// This error only occurs when assistance request is initiated by user himself.
+        /// </remarks>
+        NoHost = 1,
+        /// <summary>
+        /// Indicates that its not possible to transition to new state.
+        /// For example an canceled request cant be transitioned to rejected state.
+        /// </summary>
+        CantTransition = 2,
+        /// <summary>
+        /// Request timed out.
+        /// </summary>
+        TimedOut = 3,
+        /// <summary>
+        /// Indicates that user does not have any pending requests.
+        /// </summary>
+        NoPending = 4,
+    }
+}
