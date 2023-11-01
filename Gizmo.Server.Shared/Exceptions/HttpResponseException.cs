dde981b95e26bc0f7efd3c8e3188aa12a54e95c8
Oleg Savelos <olegsavelos@gmail.com>
2023-11-01 13:47:22 +0200

    new exception.

---
+++ Gizmo.Server.Shared/Exceptions/HttpResponseException.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using Gizmo.Exceptions;
+
+namespace Gizmo.Server.Exceptions
+{
+    /// <summary>
+    /// Custom exception used to communicate http errors to the exception filter.
+    /// </summary>
+    /// <remarks>
+    /// This will be used in order for us to be able to produce wrapped response from an exception called in one of the services api controllers might use.<br></br>
+    /// The only current use is unnauthorized access.
+    /// </remarks>
+    [ExceptionFilterCode(ExceptionCode.HttpException)]
+    public sealed class HttpResponseException : ErrorCodeExceptionBase<HttpStatusCode>
+    {
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        /// <param name="httpStatusCode">Http status code.</param>
+        public HttpResponseException(HttpStatusCode httpStatusCode):base(httpStatusCode) 
+        {
+        }
+
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        /// <param name="httpStatusCode">Http status code.</param>
+        /// <param name="message">Message.</param>
+        public HttpResponseException(HttpStatusCode httpStatusCode, string message) : base(message, httpStatusCode)
+        {
+        }
+    }
+}
