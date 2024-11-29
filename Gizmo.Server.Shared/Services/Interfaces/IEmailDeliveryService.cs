1ca0f82e39f4f5514b5133b240ce908ddb37780f
Oleg Savelos <olegsavelos@gmail.com>
2024-11-29 19:42:23 +0200

    updates.

--- Gizmo.Server.Shared/Services/Interfaces/IEmailDeliveryService.cs
+++ Gizmo.Server.Shared/Services/Interfaces/IEmailDeliveryService.cs
@@ -5,16 +5,10 @@
     /// </summary>
     public interface IEmailDeliveryService
     {
-        #region PROPERTIES
-
         /// <summary>
         /// Gets if service can currently dispatch messages.
         /// </summary>
         bool CanDispatch { get; }
-
-        #endregion
-
-        #region FUNCTIONS
 
         /// <summary>
         /// Sends a mail.
@@ -22,13 +16,11 @@
         /// <param name="html">Html body to include in mail.</param>
         /// <param name="mailSender">Address of the sender.</param>
         /// <param name="senderName">Name of the sender.</param>
-        /// <param name="recepient">Address of the recipient.</param>
-        /// <param name="recepientName">Name of the recipient.</param>
+        /// <param name="recipient">Address of the recipient.</param>
+        /// <param name="recipientName">Name of the recipient.</param>
         /// <param name="subject">Subject of the mail.</param>
-        /// <param name="attachements">List of MailAttachement to attach to mail.</param>
+        /// <param name="attachments">List of MailAttachment to attach to mail.</param>
         /// <param name="ct">CancellationToken</param>
-        Task SendMailAsync(string html, string mailSender, string senderName, string recepient, string recepientName, string subject, List<MailAttachement> attachements, CancellationToken ct = default);
-
-        #endregion
+        Task SendMailAsync(string html, string mailSender, string senderName, string recipient, string recipientName, string subject, List<MailAttachement> attachments, CancellationToken ct = default);
     }
 }
