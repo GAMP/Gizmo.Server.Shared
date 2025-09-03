f8e398072c45584471a123d0ee66395f27c329ae
Oleg Savelos <olegsavelos@gmail.com>
2025-09-03 18:41:09 +0300

    discount related updates.

---
+++ Gizmo.Server.Shared/Exceptions/CartException.cs
@@ -0,0 +1,19 @@
+﻿using Gizmo.Exceptions;
+
+namespace Gizmo.Server.Exceptions
+{
+    /// <summary>
+    /// Cart exception.
+    /// </summary>
+    [ExceptionFilterCode(ExceptionCode.Cart)]
+    public sealed class CartException : ErrorCodeExceptionBase<CartErrorCode>
+    {
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        /// <param name="errorCode">Error code.</param>
+        public CartException(CartErrorCode errorCode) : base(errorCode)
+        {
+        }
+    }
+}
---
+++ Gizmo.Server.Shared/Exceptions/Errors/CartErrorCode.cs
@@ -0,0 +1,15 @@
+﻿namespace Gizmo.Server.Exceptions
+{
+    public enum CartErrorCode
+    {
+        /// <summary>
+        /// Specified cart id is invalid.
+        /// </summary>
+        InvalidCartId = 0,
+
+        /// <summary>
+        /// Action is not supported.
+        /// </summary>
+        NotSupported = 1,
+    }
+}
