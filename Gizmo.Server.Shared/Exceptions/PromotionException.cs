c2fde6f0a55320f51e8b744562d588f5bfe6be39
Oleg Savelos <olegsavelos@gmail.com>
2025-09-08 17:45:10 +0300

    promotion exception support.

--- Gizmo.Server.Shared/Exceptions/PromotionException.cs
+++ Gizmo.Server.Shared/Exceptions/PromotionException.cs
@@ -5,6 +5,7 @@ namespace Gizmo.Server.Exceptions
     /// <summary>
     /// Promotion exception.
     /// </summary>
+    [ExceptionFilterCode(ExceptionCode.Promotion)]
     public sealed class PromotionException : ErrorCodeExceptionBase<PromotionErrorCode>
     {
         public PromotionException(PromotionErrorCode code) : base(code) { }
