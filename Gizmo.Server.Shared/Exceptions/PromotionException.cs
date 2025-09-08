3201a9d7ffcd01fa5a6c3226772830f25163f730
Oleg Savelos <olegsavelos@gmail.com>
2025-09-08 11:27:02 +0300

    error codes.

---
+++ Gizmo.Server.Shared/Exceptions/PromotionException.cs
@@ -0,0 +1,12 @@
+﻿using Gizmo.Exceptions;
+
+namespace Gizmo.Server.Exceptions
+{
+    /// <summary>
+    /// Promotion exception.
+    /// </summary>
+    public sealed class PromotionException : ErrorCodeExceptionBase<PromotionErrorCode>
+    {
+        public PromotionException(PromotionErrorCode code) : base(code) { }
+    }
+}
